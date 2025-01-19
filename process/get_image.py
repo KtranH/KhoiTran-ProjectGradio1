dfd149401481e20dac64ac8b344c6bdb9f48b43d
KtranH <87740741+KtranH@users.noreply.github.com>
2025-01-19 08:27:27 +0700

    Update code

--- process/get_image.py
+++ process/get_image.py
@@ -6,11 +6,27 @@ import os
 ###    latest_image = os.path.join(folder, image_files[-1]) if image_files else None
 ###    return latest_image
 
+### def get_latest_image(folder):
+###    try:
+###        files = os.listdir(folder)
+###        if not files:
+###            return None
+###        latest_image = os.path.join(folder, max(files))
+###        return latest_image
+###        
+###    except Exception as e:
+###        print(f"Lỗi khi lấy ảnh: {e}")
+###        return None
+
 def get_latest_image(folder):
     try:
-        files = os.listdir(folder)
+        # Lấy các file có format đúng
+        files = [f for f in os.listdir(folder) 
+                if f.startswith('OutputImage_LivePortait_') and f.endswith('.png')]
         if not files:
             return None
+        
+        # Lấy file có timestamp lớn nhất
         latest_image = os.path.join(folder, max(files))
         return latest_image
         

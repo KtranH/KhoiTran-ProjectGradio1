76747528c9e39194e1f6fb22dce984cc6774dac4
KtranH <87740741+KtranH@users.noreply.github.com>
2025-01-22 21:00:14 +0700

    Update code text to image

--- process/genImage.py
+++ process/genImage.py
@@ -35,6 +35,7 @@ def generate_image_LivePortait(input_image, rotate_pitch, rotate_yaw, rotate_rol
         with open("json/Live_Portait.json", "r", encoding="utf-8") as file_json:
             prompt = json.load(file_json)
 
+            # Tùy chỉnh thông số tạo ảnh
             prompt["17"]["inputs"]["image"] = os.path.join(INPUT_DIR, "InputImage_LivePortait_" + current_time + ".png")
             prompt["16"]["inputs"]["file_path"] = os.path.join(OUTPUT_DIR_LIVEPORTAIT, "OutputImage_LivePortait_" + current_time + ".png")
             prompt["14"]["inputs"]["rotate_pitch"] = rotate_pitch
@@ -60,15 +61,11 @@ def generate_image_LivePortait(input_image, rotate_pitch, rotate_yaw, rotate_rol
 
             # Bắt đầu quá trình tạo ảnh
             previous_image = get_latest_image_LivePortait(OUTPUT_DIR_LIVEPORTAIT)
-            if previous_image is None:
-                return np.array(Image.open(error_image_path)) 
             start_queue(prompt)
 
             global elapsed_time
             while True:
                 latest_image = get_latest_image_LivePortait(OUTPUT_DIR_LIVEPORTAIT)
-                if latest_image is None:
-                    return np.array(Image.open(error_image_path))
                 if latest_image != previous_image:
                     return np.array(Image.open(latest_image))
                 
@@ -83,30 +80,35 @@ def generate_image_LivePortait(input_image, rotate_pitch, rotate_yaw, rotate_rol
         return np.array(Image.open(error_image_path))
 
 # Hàm tạo ảnh cho chức năng tạo ảnh bằng văn bản
-def generate_image_TextToImage(seed, text, width, height):
+def generate_image_TextToImage(seed, text, width, height, model):
     error_image_path = os.path.join(ERROR, "error.jpg")
     current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
     try:
         with open("json/Text_2_Img.json", "r", encoding="utf-8") as file_json:
             prompt = json.load(file_json)
 
+            # Tùy chỉnh thông số tạo ảnh
             prompt["12"]["inputs"]["noise_seed"] = seed
             prompt["22"]["inputs"]["text_a"] = text
             prompt["11"]["inputs"]["width"] = width
             prompt["11"]["inputs"]["height"] = height
             prompt["29"]["inputs"]["file_path"] = os.path.join(OUTPUT_DIR_TEXT2IMAGE, "OutputImage_TextToImage_" + current_time + ".png")
 
+            # Căn chỉnh phong cách tạo ảnh
+            if model == "Chân thực":
+                prompt["22"]["inputs"]["text_b"] = "realistic"
+            elif model == "Hoạt hình":
+                prompt["22"]["inputs"]["text_b"] = "anime"
+            else:
+                prompt["22"]["inputs"]["text_b"] = "3D cute"
+
             # Bắt đầu quá trình tạo ảnh
             previous_image = get_latest_image_TextToImage(OUTPUT_DIR_TEXT2IMAGE)
-            if previous_image is None:
-                return np.array(Image.open(error_image_path))
             start_queue(prompt)
 
             global elapsed_time
             while True:
                 latest_image = get_latest_image_TextToImage(OUTPUT_DIR_TEXT2IMAGE)
-                if latest_image is None:
-                    return np.array(Image.open(error_image_path))
                 if latest_image != previous_image:
                     return np.array(Image.open(latest_image))
                 
--- tab_gradio/text2image.py
+++ tab_gradio/text2image.py
@@ -1,4 +1,6 @@
 import gradio as gr
+from process.create_seed import update_seed
+from process.genImage import generate_image_TextToImage
 
 css = """
     button#seed_btn {
@@ -7,12 +9,14 @@ css = """
 }
 """
 
+def check_inputs(prompt_text, seed_number):
+    return gr.Button(interactive=True) if prompt_text != "" and seed_number != "" else gr.Button(interactive=False)
 def tab1_interface():
     with gr.Blocks(css=css) as tab1:
         with gr.Row():
             with gr.Column():
                 model_img = gr.Dropdown(label="Gợi ý phong cách tạo ảnh", choices=["Chân thực", "Hoạt hình", "3D cute"], interactive=True)
-                prompt_text = gr.Textbox(label="Mô tả của bạn", placeholder="Ngoài biển, cát... (Chưa có bộ lọc ảnh NSFW. Vui lòng không nhập từ nhậy cảm)", lines=5)
+                prompt_text = gr.Textbox(label="Mô tả của bạn", placeholder="a girl, cute girl, simple background, white background... (Chưa có bộ lọc ảnh NSFW. Vui lòng không nhập từ nhậy cảm))", lines=5)
                 width_slider = gr.Slider(label="Chiều rộng", value=512, maximum=1024, minimum=256, step=64)
                 height_slider = gr.Slider(label="Chiều cao", value=768, maximum=1024, minimum=256, step=64)
                 seed_number = gr.Number(label="Thông số Seed", value=99123456999, interactive=True)
@@ -20,4 +24,25 @@ def tab1_interface():
             with gr.Row():
                 output_image = gr.Image(label="Ảnh đầu ra", height=500, width=768, interactive=False)
         submit_btn = gr.Button("Tạo ảnh 📷", interactive=False)
+    
+    # Xử lý nút tạo seed
+    random_seed_btn.click(
+        fn=update_seed, 
+        inputs=[], 
+        outputs=[seed_number]
+    )
+
+    # Kiểm tra đầu vào
+    prompt_text.change(
+        fn=check_inputs, 
+        inputs=[prompt_text, seed_number], 
+        outputs=[submit_btn]
+    )
+
+    # Xử lý nút tạo ảnh
+    submit_btn.click(
+        fn=generate_image_TextToImage, 
+        inputs=[seed_number, prompt_text, width_slider, height_slider, model_img], 
+        outputs=output_image
+    )
     return tab1
